cae8ce223aa2db75e6e2a3100a9d8cfb0e5a5605
Alex Yao <alex.yao@hyatt.com>
2022-07-20 10:25:31 +0800

    Initialize the project

---
+++ src/main/java/net/aplat/pb/PictureBrowserApplication.java
@@ -0,0 +1,13 @@
+package net.aplat.pb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureBrowserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PictureBrowserApplication.class, args);
+    }
+
+}
