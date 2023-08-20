da83d6e448b2926093bbed747812cf94115188d8
Alex <sd7y@163.com>
2023-08-20 20:59:50 +0800

    Removed test pages

--- auth/src/main/java/net/aplat/auth/config/SecurityConfig.java
+++ auth/src/main/java/net/aplat/auth/config/SecurityConfig.java
@@ -28,11 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login.html") // 登录页面设置
                 .loginProcessingUrl("/user/login") // 登录访问路径, 只是一个路径, 逻辑会由 SpringSecurity 处理
                 .defaultSuccessUrl("/login_success.html").permitAll() // 登录成功后跳转的地址
-                .and().authorizeRequests().antMatchers("/", "/test/hello", "/user/login").permitAll() // 访问这些路径的时候不需要认证
-                .antMatchers("/test/index").hasAuthority("admins") // 只有 admins 权限的才能访问
-                .antMatchers("/test/index2").hasAnyAuthority("admins", "manager") // admins 和 manager 都能访问
-                .antMatchers("/test/index3").hasRole("sale") // 有 sale 角色, 但是设置角色的时候需要设置 ROLE_sale 才行, 源码中是加了个 ROLE_ 前缀了
-                .antMatchers("/test/index4").hasAnyRole("sale1", "sale2") // 有 sale 或 sale2 才能访问
+                .and().authorizeRequests().antMatchers("/", "/user/login").permitAll() // 访问这些路径的时候不需要认证
                 .anyRequest().authenticated()
                 .and().csrf().disable(); // 关闭 csrf 防护
 
